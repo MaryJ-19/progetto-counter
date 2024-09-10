a3e64a6e7f3fc544a98877af7edfd42f37abefd2
Maria Carta <mmarij191@gmail.com>
2024-09-10 18:05:53 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+
+
+let counter = document.querySelector('.counter');
+let buttons = document.querySelector('.buttons-wrapper')
+
+// creazione elementi 
+
+let number = document.createElement('h1')
+number.innerText = '0'
+counter.appendChild(number)
+
+const lessButton = document.createElement('button')
+lessButton.innerText = '-'
+buttons.appendChild(lessButton)
+
+const resetButton = document.createElement('button')
+resetButton.innerText = 'reset'
+buttons.appendChild(resetButton)
+
+const moreButton = document.createElement('button')
+moreButton.innerText = '+'
+buttons.appendChild(moreButton)
+
+// funzioni 
+
+resetButton.addEventListener('click', () => {
+    number.innerText = '0'
+})
+
+moreButton.addEventListener('click', () => {
+    number.innerText = Number(number.innerText) + 1
+})
+
+lessButton.addEventListener('click', () => {
+
+    if (number.innerText != 0) {
+        number.innerText = Number(number.innerText) - 1;
+    }
+})
+
